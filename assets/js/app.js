1322371ca96b10d16fb3d861a7c827469c810da4
Ken Petti <kennethpetti@gmail.com>
2015-02-26 13:56:15 +0000

    report creation, sorting, and filtering all working as expected

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,16 +30,34 @@ define([
         }
     })
 
+    var ReportLocation = Backbone.Model.extend({
+        urlroot: '/location',
+    })
+
+    var Reporter = Backbone.Model.extend({
+        urlroot: '/reporter',
+    })
+
 // Collection
+    var LocationList = Backbone.Collection.extend({
+        url: '/location',
+        model: ReportLocation,
+    })
+
+    var ReporterList = Backbone.Collection.extend({
+        url: '/reporter',
+        model: Reporter,
+    })
+
     var ReportList = Backbone.Collection.extend({
         url: '/report',
         model: Report,
-      defaultSortProp: 'createdAt',
-      defaultSortDir: 'desc',
+        defaultSortProp: 'createdAt',
+        defaultSortDir: 'desc',
 
-      initialize: function () {
-        this.comparator = this.getSortStrategy(this.defaultSortProp, this.defaultSortDir);
-      },
+        initialize: function () {
+          this.comparator = this.getSortStrategy(this.defaultSortProp, this.defaultSortDir);
+        },
 
         getSortStrategy: function (property, direction) {
           var dirMod = ((direction === 'asc') ? 1 : -1);
@@ -71,25 +89,41 @@ define([
         template: _.template($('#report_template').html()),
 
         render: function () {
-            console.log(this.model)
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         }
     })
 
-    var reportEntryView = Backbone.View.extend({
+    var ReportEntryView = Backbone.View.extend({
+        el: '#report_entry_anchor',
         events: {
           'click #submit_report'   : 'submitReport',
         },
 
         template: _.template($('#report_entry_template').html()),
 
+        initialize: function () {
+            _.bindAll(this, 'render', 'submitReport');
+            this.locations = new LocationList()
+            this.reporters = new ReporterList()
+            $.when(this.locations.fetch({reset: true}),
+                   this.reporters.fetch({reset: true}))
+            .then(this.render);
+
+
+        },
+
         render: function () {
-            this.$el.append(this.template());
+            this.$el.append(this.template({locations: this.locations.toJSON(), reporters: this.reporters.toJSON()}));
             return this;
         },
 
-        submitReport: function () {
+      remove: function() {
+         this.$el.children('#report_entry_container').remove()
+      },
+      
+        submitReport: function (event) {
+          event.stopPropagation();
           var self = this;
               form = $('#report_entry'),
               url = document.domain + ':' + location.port,
@@ -121,7 +155,7 @@ define([
             _.bindAll(this, 'render');
             this.reportListElement = this.$el.children('#report_list');
             this.collection.on('reset', this.render, this);
-          this.collection.on('sort', function () {
+            this.collection.on('sort', function () {
               this.reportListElement.empty();
               this.render();
             }, this);
@@ -144,41 +178,39 @@ define([
             return this
         },
 
-      changeOrder: function (event) {
-        var property = $('#sort_prop').val(),
-            direction = $('#sort_dir').val(),
-            reports = this.collection;
+        changeOrder: function (event) {
+          var property = $('#sort_prop').val(),
+              direction = $('#sort_dir').val(),
+              reports = this.collection;
 
-        reports.comparator = reports.getSortStrategy(property, direction);
-        reports.sort();
-      }
-    })
+          reports.comparator = reports.getSortStrategy(property, direction);
+          reports.sort();
+          }
+    });
 
     var AppRouter = Backbone.Router.extend({
 
-        routes:{
-            '':'dashboard',
-            '#':'dashboard',
-            'reporter/:id':'reporterReports',
-            'location/:id':'locationReports',
-            'report_entry':'createReport'
+        routes: {
+          '':'dashboard',
+          '#':'dashboard',
+          'reporter/:id':'reporterReports',
+          'location/:id':'locationReports',
+          'report_entry':'createReport'
         },
 
         dashboard: function () {
-            if (!this.reportList && !this.reportListView) {
-              this.reportList = new ReportList();
-              this.reportListView = new ReportListView({collection:this.reportList});
-              this.reportList.fetch({reset: true});
-            } else if (this.reportList && this.reportListView) {
-              this.reportListView.reportListElement.empty()
-              this.reportListView = new ReportListView({collection:this.reportList});
-            }
-
             if (this.reportEntryView) {
               this.reportEntryView.remove();
             }
+          
+            if (this.reportListView) {
+              this.reportListView.reportListElement.empty()              
+            }
+          
+            this.reportList = new ReportList();
+            this.reportListView = new ReportListView({collection:this.reportList});
+            this.reportList.fetch({reset: true})
 
-            this.reportListView.render();
             $('#dashboard_link').hide();
             $('#create_report').show();
         },
@@ -204,13 +236,12 @@ define([
         },
 
         createReport: function() {
-           this.reportEntryView = new reportEntryView();
-           $('#dashboard_link').show();
-           $('#create_report').hide();
+            this.reportEntryView = new ReportEntryView();
+            $('#dashboard_link').show();
+            $('#create_report').hide();
 
-           $('#dashboard').append(this.reportEntryView.render().el);
+//             $('#dashboard').append(this.reportEntryView.render().el);
         }
-
     });
 
     var app = new AppRouter();
