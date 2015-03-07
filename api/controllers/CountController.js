849f93adadae7571b7ef58a387c36377777430ae
Ken Petti <kennethpetti@gmail.com>
2015-03-07 02:16:48 +0000

    updated backend

---
+++ api/controllers/CountController.js
@@ -0,0 +1,21 @@
+/**
+ * CountController
+ *
+ * @description :: Server-side logic for managing counts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `CountController.composition()`
+   */
+  composition: function (req, res) {
+    return res.json({
+      todo: 'composition() is not implemented yet!'
+    });
+  }
+};
+
---
+++ api/models/Report.js
@@ -0,0 +1,19 @@
+/**
+* Report.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    reporter    : { model: 'reporter', required: true },
+    location    : { model: 'location', required: true },
+    line        : { type: 'boolean', required: true, defaultsTo: true },
+    count       : { type: 'integer', required: true, defaultsTo: 0},
+    composition : { type: 'integer', required: true, defaultsTo: 3}
+  },
+
+};
+
