faa89303bac7fa0576028d418727769df344a334
Ken Petti <kennethpetti@gmail.com>
2015-03-28 16:11:47 +0000

    adding bower

---
+++ tasks/config/bower.js
@@ -0,0 +1,24 @@
+/**
+ * Copy bower files to .tmp pubic file
+ *
+ * ---------------------------------------------------------------
+ *
+ * This grunt task is configured to clean out the contents in the .tmp/public of your
+ * sails project.
+ *
+ * For usage docs see:
+ * 		https://github.com/gruntjs/grunt-contrib-clean
+ */
+
+module.exports = function(grunt) {
+  grunt.config.set('bower', {
+    dev: {
+        dest: '.tmp/public',
+        js_dest: '.tmp/public/js',
+        css_dest: '.tmp/public/styles'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bower');
+
+};
