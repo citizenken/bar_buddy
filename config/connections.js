9d6dec26ed37828392c553033341d0ee5ee8fe50
Ken Petti <ken.petti@cloudlock.com>
2015-05-14 23:41:02 -0400

    adding connection settings

--- config/connections.js
+++ config/connections.js
@@ -42,6 +42,10 @@ module.exports.connections = {
   ***************************************************************************/
   mysqlServer: {
     adapter: 'sails-mysql'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   },
 
   /***************************************************************************
