285ab9f3a51d2facaa2413140bc415d5eee55aa6
Ken Petti <ken.petti@cloudlock.com>
2015-05-22 23:40:23 -0400

    touching up ui and report display

--- assets/js/app/router.js
+++ assets/js/app/router.js
@@ -2,15 +2,15 @@ barBuddyApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/', {
-        templateUrl: 'partials/dashboard.html',
+        templateUrl: '/partials/dashboard.html',
         controller: 'DashboardCtrl'
       }).
       when('/location/:locationId', {
-        templateUrl: 'partials/location.html',
+        templateUrl: '/partials/location.html',
         controller: 'LocationDetailCtrl'
       }).
       when('/reporter/:reporterId', {
-        templateUrl: 'partials/reporter.html',
+        templateUrl: '/partials/reporter.html',
         controller: 'ReporterDetailCtrl'
       }).
       otherwise({
