2cf874cf3f12135c54dcb9558b8d730cdfa137f7
Ken Petti <ken.petti@cloudlock.com>
2015-07-20 07:45:29 -0400

    debugging query string

--- api/policies/bearerAuth.js
+++ api/policies/bearerAuth.js
@@ -13,7 +13,7 @@
  */
 
 module.exports = function (req, res, next) {
-  sails.log.debug(req.params);
+  console.log(req.query);
   return passport.authenticate('bearer', { session: false })(req, res, next);
 
 };
